b87d31ae2eb33dd4b2a1ee66efdffdd5918adf0d
Michael Bumann <hello@michaelbumann.com>
2022-03-23 15:37:20 +0100

    Simple authentication

--- grpc/index.js
+++ grpc/index.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const express = require("express");
 const lnService = require("ln-service");
+const cookieParser = require("cookie-parser");
 
 require("dotenv").config();
 
@@ -17,6 +18,7 @@ lnService.getIdentity({ lnd }, (error, result) => {
 
 const app = express();
 
+app.use(cookieParser());
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
@@ -24,22 +26,29 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", async (req, res) => {
   const invoice = await lnService.createInvoice({
     lnd,
-    tokens: 100,
+    tokens: 10,
     description: "bolt.fun rocks",
   });
 
   console.log(invoice);
+  res.cookie("_ln_id", invoice.id);
   res.render("index", { invoice });
 });
 
-app.post("/invoice", async function (req, res) {
-  const { invoice, params, successAction, validatePreimage } =
-    await lnurl.requestInvoice({
-      lnUrlOrAddress: RECIPIENT,
-      tokens: AMOUNT,
-    });
-  const requestDetails = parsePaymentRequest({ request: invoice });
-  res.json({ payment_request: invoice, payment_hash: requestDetails.id });
+app.get("/authenticated", async (req, res) => {
+  console.log(req.cookies);
+  const id = req.cookies._ln_id;
+  console.log(id);
+  const invoice = await lnService.getInvoice({
+    lnd,
+    id,
+  });
+
+  if (invoice.is_confirmed) {
+    res.render("success");
+  } else {
+    res.redirect("/");
+  }
 });
 
 const port = process.env.PORT || 3030;
